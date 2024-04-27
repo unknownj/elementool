ee459a8f22d9a5fbaf400d3af67c5d8632d95793
James Webley <webley@gmail.com>
2024-04-27 08:48:32 +0100

    Testing bits

--- index.test.js
+++ index.test.js
@@ -1,28 +1,32 @@
 const { JSDOM } = require('jsdom');
-const dom = new JSDOM();
+const fs = require('fs');
+const dom = new JSDOM(`
+<!DOCTYPE html>
+<body>
+  <script>${fs.readFileSync("index.js").toString()}</script>
+</body>`, { runScripts: 'dangerously' });
 global.document = dom.window.document;
 global.Element = dom.window.Element;
 
-const YourObject = require('./index'); // replace with the actual exported object containing the append method
 
 describe('append function', () => {
   let element;
-  let yourObject;
+  let el;
 
   beforeEach(() => {
     element = document.createElement('div');
-    yourObject = new YourObject(); // replace with the actual constructor
+    el = new dom.window.Elementool(); // replace with the actual constructor
   });
 
   test('appends a single element', () => {
     const child = document.createElement('p');
-    yourObject.append(element, child);
+    el.append(element, child);
     expect(element.firstChild).toBe(child);
   });
 
   test('appends an array of elements', () => {
     const children = [document.createElement('p'), document.createElement('span')];
-    yourObject.append(element, children);
+    el.append(element, children);
     children.forEach((child, index) => {
       expect(element.children[index]).toBe(child);
     });
@@ -30,7 +34,7 @@ describe('append function', () => {
 
   test('ignores non-Element values in an array', () => {
     const children = [document.createElement('p'), 'not an element', document.createElement('span')];
-    yourObject.append(element, children);
+    el.append(element, children);
     expect(element.children.length).toBe(2);
     expect(element.firstChild).toBe(children[0]);
     expect(element.lastChild).toBe(children[2]);
